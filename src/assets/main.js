26fb18c16883c38771c4e7b0870e84098fe1012c
Erick Rojas <erickrojaz@gmail.com>
2017-08-18 14:57:49 -0500

    Fetching my codeschool badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/erickthered.json',
+        dataType: 'jsonp',
+        success: function(response) {
+          console.log(response.courses.completed);
+          for (i =0; i< response.courses.completed.length; i++) {
+            var course = response.courses.completed[i];
+            var newElement = $('<div></div>');
+
+            newElement.addClass('course');
+            newElement.html('<h3>' + course.title + '</h3>');
+            newElement.append('<img src="' + course.badge + '">')
+            newElement.append('<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>')
+            $('#badges').append(newElement);
+          }
+        }
+      });
 
 });
